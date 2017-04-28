11a9d6f6c806e9eeb8640d07634559de8e78675c
Maya Nair <msnair@emory.edu>
2017-04-28 18:32:07 -0400

    Update news_api_requests.py

--- news_api_requests.py
+++ news_api_requests.py
@@ -2,15 +2,14 @@
 import requests
 
 # Constants
-NEWS_API_KEY = 1c9c265acc9a44fd8e597a83b238896a
+NEWS_API_KEY = '1c9c265acc9a44fd8e597a83b238896a'
 NEWS_URL = 'https://newsapi.org/v1/articles'
 
-def suggested_articles(source,apiKey,sortBy):
+def suggested_articles(source,apiKey):
     # Package params into dictionary for GET request.
     params = {
         'key':NEWS_API_KEY, #ASK ROBERT if we need to restate api key here if its already defined above!!!!!!!
-        's': source,
-        'sorting': sortBy
+        's': source
     }
 
     # Send POST request to Google.
