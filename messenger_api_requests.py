897793ab5684d7f1b74d0a96162da5eb266e91c2
Maya Nair <msnair@emory.edu>
2017-04-28 18:33:07 -0400

    Update messenger_api_requests.py

--- messenger_api_requests.py
+++ messenger_api_requests.py
@@ -4,7 +4,7 @@ import requests
 
 # Constants
 FB_ACCESS_TOKEN = 'EAADtxV351YoBAP5Ue6AU9KgkMdKjF4n9FQ6RSMWWGIS2pZAqagRIpSwQLL8De3QYIXWHpPoNEKBOl0m6YkeJaHdohBWhw96e5vY7azuXZBtDhfZBe5mcgVNDM2CphZA0r5ZBdfxAw9CiPNif9a9lvLbhWb2PsDflSKMVhYzcwGwZDZD'
-SEND_API_URL = 'https://graph.facebook.com/v2.6/me/messages?access_token=' + FB_ACCESS_TOKEN
+SEND_API_URL = 'https://graph.facebook.com/v2.6/me/messages?access_token=EAADtxV351YoBAP5Ue6AU9KgkMdKjF4n9FQ6RSMWWGIS2pZAqagRIpSwQLL8De3QYIXWHpPoNEKBOl0m6YkeJaHdohBWhw96e5vY7azuXZBtDhfZBe5mcgVNDM2CphZA0r5ZBdfxAw9CiPNif9a9lvLbhWb2PsDflSKMVhYzcwGwZDZD'
 
 def send_message(messenger_id, text):
     # Package params into dictionaries for POST request
