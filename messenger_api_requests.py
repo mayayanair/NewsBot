52f4b86f3acb675f87a0c025a6c38d81d7ddc94d
Maya Nair <msnair@emory.edu>
2017-04-28 18:32:36 -0400

    Update messenger_api_requests.py

--- messenger_api_requests.py
+++ messenger_api_requests.py
@@ -3,7 +3,7 @@
 import requests
 
 # Constants
-FB_ACCESS_TOKEN = 'YOUR ACCESS TOKEN'
+FB_ACCESS_TOKEN = 'EAADtxV351YoBAP5Ue6AU9KgkMdKjF4n9FQ6RSMWWGIS2pZAqagRIpSwQLL8De3QYIXWHpPoNEKBOl0m6YkeJaHdohBWhw96e5vY7azuXZBtDhfZBe5mcgVNDM2CphZA0r5ZBdfxAw9CiPNif9a9lvLbhWb2PsDflSKMVhYzcwGwZDZD'
 SEND_API_URL = 'https://graph.facebook.com/v2.6/me/messages?access_token=' + FB_ACCESS_TOKEN
 
 def send_message(messenger_id, text):
@@ -18,16 +18,23 @@ def send_message(messenger_id, text):
     # Send POST request to Facebook Messenger Send API to send text message
     r = requests.post(SEND_API_URL, json=params)
     
-def send_categories_message(messenger_id, text):
+def send_categories_message(messenger_id, text, categories):
     # Package params into dictionaries for POST request
+    quick_replies = [] # append to
+    for c in categories:
+        # https://developers.facebook.com/docs/messenger-platform/send-api-reference/quick-replies
+        '''
+        {
+            "content_type":"text",
+            "title":"CATEGORY NAME",
+            "payload":"CATEGORY NAME"
+        }
+        '''
+
     recipient = {'id':messenger_id}
     message = {
         'text':text,
-        'quick_replies':[
-            {
-                'content_type':'location' #what would this manifest as from the user's perspective
-            }
-        ]
+        'quick_replies':quick_replies
     }
     params = {
         'recipient':recipient,
