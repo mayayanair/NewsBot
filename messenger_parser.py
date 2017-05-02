9ad6b53138b3d529f184a1779b0389e0baaaf8bd
Maya Nair <msnair@emory.edu>
2017-05-02 16:54:58 -0400

    Update messenger_parser.py

--- messenger_parser.py
+++ messenger_parser.py
@@ -12,12 +12,4 @@ class MessengerParser:
         # Grab message body out of JSON
         message = message_body['message']
 
-        # Check if the message body has an attachment, this means we are receiving coordinates (https://developers.facebook.com/docs/messenger-platform/send-api-reference/quick-replies)
-        #if 'attachments' in message:
-           # attachments = message['attachments']
-           # coordinates = attachments[0]['payload']['coordinates']
-           # self.lat = coordinates['lat']
-           # self.lng = coordinates['long']
-        # else:
-            # Normal text message
         self.text = message['text']
