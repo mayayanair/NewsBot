e1200bd6212627c5fc750634acf7b4b55bdabbc2
Maya Nair <msnair@emory.edu>
2017-05-02 16:55:40 -0400

    Update news_api_requests.py

--- news_api_requests.py
+++ news_api_requests.py
@@ -1,23 +1,23 @@
-# Script to send API requests to Google API's
+# Script to send API requests to News API
 import requests
 
 # Constants
 NEWS_API_KEY = '1c9c265acc9a44fd8e597a83b238896a'
 NEWS_URL = 'https://newsapi.org/v1/articles'
 
-def suggested_articles(source,apiKey):
+def articles(source):
     # Package params into dictionary for GET request.
     params = {
-        'key':NEWS_API_KEY, #ASK ROBERT if we need to restate api key here if its already defined above!!!!!!!
-        's': source
+        'apiKey':NEWS_API_KEY,
+        'source': source
     }
 
-    # Send POST request to Google.
+    # Send POST request to News API
     r = requests.get(NEWS_URL, params=params)
 
     # Check if success, if success, return translated text. If not, return error message.
     if r.status_code != 200:
         return 'Something wrong happened, try again later!'
 
-    text = r.json()['data']['translations'][0]['translatedText'] # ASK ROBERT what this will return !!!!!!!!!!!
+    text = r.json()['articles']
     return text
